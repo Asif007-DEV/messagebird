f024f48d24a26b4c4a5280c229cab2b7bf0bf105
Asif007-DEV <73417008+Asif007-DEV@users.noreply.github.com>
2020-11-10 14:15:31 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,34 @@
+router.post('/phone',(req,res)=>{
+    const number = req.body.number;
+    messageBird.verify.create(number,{
+        template:'your verification code is %token'
+    },function(err,response){
+        if(err){
+            console.log(err);
+            res.status(400).send({error:err.essors[0].description});
+        }else{
+            console.log(response);
+            res.status(200).send({
+                id:response.id
+            });
+        }
+    })
+})
+
+router.post('/verify',(req,res)=>{
+    const id = req.body.id;
+    const token = req.body.token;
+    console.log(id);
+    messageBird.verify.verify(id,token,function(err,response){
+        if(err){
+            console.log(err);
+            res.status(422).send({error:err.errors[0].description});
+        }else{
+            console.log(response);
+            res.status(200).send({
+                success:"your number is verify"
+            });
+        }
+    });
+})
+
